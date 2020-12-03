8f497727a8484056ac6465eb0d3e7309ad19c88b
orangedays00 <orangedays8402@gmail.com>
2020-12-03 23:55:11 +0900

    APIキー削除

--- js/main.js
+++ js/main.js
@@ -213,7 +213,7 @@ function sendMail(){
             "method": "POST",
             "headers": {
                 "x-rapidapi-host": "rapidprod-sendgrid-v1.p.rapidapi.com",
-                "x-rapidapi-key": "2c945c3dc7msh925e8e18da683ecp18839bjsnbea8973f7c5b",
+                "x-rapidapi-key": "",
                 "content-type": "application/json",
                 "accept": "application/json"
             },
@@ -248,108 +248,3 @@ function sendMail(){
         });
     })
 }
-
-
-
-
-// -----------------------
-// Gmail APIの実装（うまく動かず）
-// -----------------------
-
-// Step 1で取得したOAuthクライアントIDをここに書く。
-// const CLIENT_ID = '562545842346-eo5c01nf567em1otdiqn7ctgqnrearl6.apps.googleusercontent.com';
-
-// async function onLoad() {
-//   try {
-//     // Google APIs Client Libraryの初期化。
-//     await gapi.load('client:auth2');
-//     await gapi.client.init({
-//         clientId: CLIENT_ID,
-//         scope: 'https://www.googleapis.com/auth/gmail.send'
-//     });
-//     await gapi.client.load('gmail', 'v1');
-//     console.log('Initialized');
-//   } catch (e) {
-//     console.error(e);
-//   }
-// }
-
-// async function signIn() {
-//   try {
-//     await gapi.auth2.getAuthInstance().signIn();
-//     console.log('Signed in');
-//   } catch (e) {
-//     console.error(e);
-//   }
-// }
-
-// async function signOut() {
-//   try {
-//     await gapi.auth2.getAuthInstance().signOut();
-//     console.log('Signed out');
-//   } catch (e) {
-//     console.error(e);
-//   }
-// }
-
-// async function sendEmail() {
-//   try {
-//     // 送りたいメールアドレスに書き換えてください。
-//     const to = 'orangedays8402@gmail.com';
-//     const subject = 'テスト';
-//     const body = 'これはテストです。';
-
-//     // サインイン済みかチェック。
-//     if (!gapi.auth2.getAuthInstance().isSignedIn.get()) {
-//       console.error('Sign in first');
-//       return;
-//     }
-
-//     // メールデータの作成。
-//     const mimeData = [
-//       `To: ${to}`,
-//       'Subject: =?utf-8?B?' + btoa(unescape(encodeURIComponent(subject))) + '?=',
-//       'MIME-Version: 1.0',
-//       'Content-Type: text/plain; charset=UTF-8',
-//       'Content-Transfer-Encoding: 7bit',
-//       '',
-//       body,
-//     ].join('\n').trim();
-//     const raw = btoa(unescape(encodeURIComponent(mimeData))).replace(/\+/g, '-').replace(/\//g, '_');
-
-//     // メールの送信。
-//     await gapi.client.gmail.users.messages.send({
-//       'userId': 'me',
-//       'resource': {raw: raw},
-//     });
-//     console.log('Sent email');
-
-//   } catch (e) {
-//     console.error(e);
-//   }
-// }
-
-//スクリーンショットを保存：
-// document.getElementById('screenShot').addEventListener('click',()=>{
-
-//     //HTML内に画像を表示
-//     html2canvas(document.getElementById('myMap'),{
-//         onrendered: function(canvas){
-//         //imgタグのsrcの中に、html2canvasがレンダリングした画像を指定する。
-//         var imgData = canvas.toDataURL();
-//         // document.getElementById("result").src = imgData;
-//         document.getElementById("result").innerText = imgData;
-//         }
-//     });
-
-    //   //ボタンを押下した際にダウンロードする画像を作る
-    //   html2canvas(document.body,{
-    //     onrendered: function(canvas){
-    //       //aタグのhrefにキャプチャ画像のURLを設定
-    //       var imgData = canvas.toDataURL();
-    //       document.getElementById("ss").href = imgData;
-    //     }
-    //   });
-
-    // });
-
